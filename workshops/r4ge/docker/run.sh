f3c6da61106acbfa0cc2bf93b0ebbc18ede39f8b
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2018-02-15 19:56:12 +0100

    better run.sh for rage

--- workshops/r4ge/docker/run.sh
+++ workshops/r4ge/docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t hackenv . && docker run -it hackenv bash
+docker build -t hackenv . && docker run --cap-add=SYS_PTRACE --security-opt=apparmor:unconfined --privileged -v `pwd`:/workshop -it hackenv bash
