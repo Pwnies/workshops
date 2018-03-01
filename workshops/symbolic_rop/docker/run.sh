0f324bf96f31eae24ded15c9d47d0ee215d64ac9
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2018-03-01 18:54:05 +0100

    added fininised workshop material

--- workshops/symbolic_rop/docker/run.sh
+++ workshops/symbolic_rop/docker/run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker build -t hackenv . && docker run --cap-add=SYS_PTRACE --security-opt=apparmor:unconfined --privileged -v `pwd`:/workshop -it hackenv bash
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=$(dirname $SCRIPT)
+
+docker build -t hackenv . && docker run --cap-add=SYS_PTRACE --security-opt=apparmor:unconfined --privileged -v $SCRIPTPATH/..:/workshop -it hackenv bash
